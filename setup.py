a45bb376e72947c9f193b23265a502edf1c639ba
Tanish Kandivlikar <tanish.kandivlikar1412@gmail.com>
2024-01-23 00:35:52 -0500

    pushing to S3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='project for end to end mlops new york taxi trip duration',
+    author='tanish',
+    license='',
+)
